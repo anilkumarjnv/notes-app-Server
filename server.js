4506b69c2e5e74f11c54d16a4e59c5284d746047
anil <anilkumar2000jnv@gmail.com>
2023-12-20 11:10:54 +0530

    commit

--- server.js
+++ server.js
@@ -4,20 +4,21 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const userRouter = require('./routes/userRouter')
 const noteRouter= require('./routes/noteRouter')
+const path = require("path");
 
 const app = express()
 app.use(express.json())
 app.use(cors())
 
 
+
+
+
+
 //Routes
 app.use('/users',userRouter)
 app.use('/api/notes',noteRouter)
 
-const PORT = process.env.PORT
-app.listen(PORT,()=>{
-    console.log("server is running on port",PORT)
-})
 
 //connect to MongoDB
 const URI = process.env.MONGODB_URL
@@ -31,3 +32,17 @@ mongoose.connect(URI, {
   console.error(`Error connecting to MongoDB: ${error.message}`);
   process.exit(1); // Exit with failure
  })
+
+ if(process.env.NODE_ENV === 'production'){
+    app.use(express.static('client/build'))
+    app.get('*',(req,res)=>{
+        res.sendFile(path.join(__dirname,'client','build','index.html'))
+    });
+ }
+
+
+ 
+const PORT = process.env.PORT || 5000
+app.listen(PORT,()=>{
+    console.log("server is running on port",PORT)
+})
